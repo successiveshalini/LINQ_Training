46dc6c5cec2fe56e192788c32dd69743456eac63
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-27 11:28:57 +0530

    LinqProgram1

---
+++ LinqProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Data source
+            string[] names = { "Bill", "Steve", "James", "Mohan" };
+
+            // LINQ Query 
+            var myLinqQuery = from name in names
+                              where name.Contains('t')
+                              select name;
+
+            // Query execution
+            foreach (var name in myLinqQuery)
+                Console.Write(name + " ");
+            Console.ReadKey();  
+        }
+    }
+}
