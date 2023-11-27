64cc35d6acff2f24143417ee652f9a38ba041349
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-27 12:51:01 +0530

    LinqStudentProgram

---
+++ LinqProgram2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProgram2
+{
+    public class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            string[] students = { "sohan", "soni", "Priti", "puja", "Rupa" };  //Data Source
+
+            var LinqQuery = from student in students
+                            where student.Contains('i') // Linq Query
+                            select student;
+
+            foreach (var student in LinqQuery)     // Query Execuation
+                Console.WriteLine(student + " ");
+            Console.ReadKey();  
+
+
+
+        }
+    }
+}
+
