06ea073254fbc5911a4cd3c23beabb5fc581eb1a
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-27 11:27:54 +0530

    LinqMethodProgram

---
+++ LinqMethodProgram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqMethodProgram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // string collection
+            IList<string> stringList = new List<string>() {
+            "C# Tutorials",
+            "VB.NET Tutorials",
+            "Learn C++ Tutorials",
+            "MVC Tutorials" ,
+            "Java"
+        };
+
+            // LINQ Method Syntax
+            var result = stringList.Where(s => s.Contains("Tutorials"));
+
+
+            foreach (var str in result)
+            {
+                Console.WriteLine(str);
+                Console.ReadKey();  
+
+            }
+        }
+    }
+}
