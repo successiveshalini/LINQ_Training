488449ff9f6919b563ae539351b77521e9ffc3d4
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-27 17:02:33 +0530

    WhereConditionProgram

---
+++ WhereConditionProgram.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhereConditionProgram
+{
+    public class Student
+    {
+
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student collection
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John", Age = 13} ,
+                new Student() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+                new Student() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+                new Student() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+            };
+
+            //var teenAgerStudent = studentList.Where(s => s.Age > 12 && s.Age < 20);
+
+            //foreach (var str in studentList)
+            //{
+            //    Console.WriteLine(str);
+            //    Console.ReadKey();  
+            //}
+
+             //LINQ Query Syntax to find out teenager students
+            var teenAgerStudent = from s in studentList
+                                  where s.Age > 18 && s.Age < 21
+                                  select s;
+            Console.WriteLine("Teen age Students:");
+
+            foreach (Student std in teenAgerStudent)
+            {
+                Console.WriteLine(std.StudentName);
+                Console.ReadKey();
+            }
+        }
+    }
+}
