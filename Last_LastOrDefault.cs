7ad27774c9e316d16fd3c50154d01ca1325e2526
successiveshalini <147502984+successiveshalini@users.noreply.github.com>
2023-11-28 16:50:44 +0530

    ThenByLinqProgram

---
+++ Last_LastOrDefault.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last_LastOrDefault
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<int> intList = new List<int>() { 7, 10, 21, 30, 45, 50, 87 };
+            IList<string> strList = new List<string>() { null, "Shalini", "Soni", "Priti", "GunGun" };
+            IList<string> emptyList = new List<string>();
+            Console.WriteLine("The Element in intList : {0}", intList.Last());
+            Console.WriteLine("The Element in strList : {0}", strList.Last());
+            //Console.WriteLine("the Element in emptyList : {0}", emptyList.Last());    
+
+            Console.WriteLine("Last Element in intList: {0}", intList.LastOrDefault());
+
+            Console.WriteLine("Last Even Element in intList: {0}",
+                              intList.LastOrDefault(i => i % 2 == 0));
+
+            Console.WriteLine("Last Element in strList: {0}", strList.LastOrDefault());
+
+            Console.WriteLine("Last Element in emptyList: {0}", emptyList.LastOrDefault());
+
+            Console.ReadKey();  
+
+
+        }
+    }
+}
---
+++ ThenByLinqProgram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThenByLinq
+{
+    public class Student
+    {
+
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "Mohan", Age = 18, Address = "Delhi" } ,
+                new Student() { StudentID = 2, StudentName = "Shyam",  Age = 15, Address = "Kolkata"} ,
+                new Student() { StudentID = 3, StudentName = "Rohan",  Age = 25, Address = "Patna"} ,
+                new Student() { StudentID = 4, StudentName = "Ram" , Age = 20, Address = "Pakistan"} ,
+                new Student() { StudentID = 5, StudentName = "Sita" , Age = 19, Address = "Dubai" }
+            };
+            var thenByResult = studentList.OrderBy(s => s.StudentName).ThenBy(s => s.Age).ThenBy(s => s.Address);
+
+            var ThenByDescending = studentList.OrderBy(s => s.StudentName).ThenByDescending(s => s.Age).ThenByDescending(s => s.Address);
+
+            Console.WriteLine("ThenBy :");
+
+            foreach (var std in thenByResult)
+                Console.WriteLine("Name: {0}, Age:{1} Address:{2}", std.StudentName, std.Age, std.Address);
+
+            Console.WriteLine("ThenByDescending:");
+
+            foreach (var std in ThenByDescending)
+                Console.WriteLine("Name: {0}, Age:{1} Address:{2}", std.StudentName, std.Age, std.Address);
+            Console.ReadKey();
+
+
+        }
+    }
+}
